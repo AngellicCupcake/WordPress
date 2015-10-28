56bfb6f5533b3dd8c42945dd2ece072597af4d2a
Aliyah Bolden <kawaiialiyah@gmail.com>
2015-10-27 20:17:20 -0400

    added in class assignment
    In class had been done and added here

---
+++ hitsujichan/InClass12.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+<?php 
+
+   /* $truth   = TRUE;
+    $truth   = 1;
+    $lie     = FALSE;
+    $lie     = 0;
+
+    $cute    = "puppycat";
+    $cat     = 'lil bub';
+    
+    $number  = 87; //integer
+    $float   = 3.14;
+
+    echo ($cat);
+    echo "<br>";
+    echo " eats ";
+    echo "<br>";
+    echo ($cute);
+    echo "<br>";
+    echo $cat. " eats " .$cute; //combines them all so that it's all in one line
+    // same as putting it underneath each other.
+
+    echo "<pre>";
+        var_dump($truth);
+        var_dump($lie);
+        var_dump($cute);
+        var_dump($cat);
+        var_dump($number);
+        var_dump($float);
+    echo "</pre>";
+
+
+function appleCounter($bucketSize){
+    
+    $x = 1;
+
+    while ($x <= $bucketSize){
+    
+       if( $x ==1 ){
+        // Do Something
+        echo $x." apple";
+        echo "<br>";
+    
+    } else {
+        // Do Something Else  
+        echo $x." apples";
+        echo "<br>";
+    }
+
+        
+        $x++;
+    
+    } // Counts 29 puppycats on a different line each time.
+    
+}
+
+
+appleCounter(10);*/
+
+
+?>
+    
+</body>
+</html>
